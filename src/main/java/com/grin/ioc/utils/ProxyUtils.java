12c2f3622e908334766053225f497097465e2016
Stanislav Kotyhrobov <workautu@gmail.com>
2021-11-25 11:27:21 +0200

    Improments ProxyUtils

--- src/main/java/com/grin/ioc/utils/ProxyUtils.java
+++ src/main/java/com/grin/ioc/utils/ProxyUtils.java
@@ -15,28 +15,11 @@ public class ProxyUtils {
     public static void createProxyInstance(ServiceDetails serviceDetails, Object[] constructorParams) {
         final ProxyFactory proxyFactory = new ProxyFactory();
         proxyFactory.setSuperclass(serviceDetails.getServiceType());
-        final Class<?> cls = proxyFactory.createClass();
 
         Object proxyInstance;
         try {
-            proxyInstance = Arrays.stream(cls.getDeclaredConstructors())
-                    .filter(ctr -> {
-                        if (ctr.getParameterCount() != constructorParams.length) {
-                            return false;
-                        }
-
-                        final Class<?>[] parameterTypes = ctr.getParameterTypes();
-
-                        for (int i = 0; i < parameterTypes.length; i++) {
-                            if (constructorParams[i] != null && !parameterTypes[i].isAssignableFrom(constructorParams[i].getClass())) {
-                                return false;
-                            }
-                        }
-
-                        ctr.setAccessible(true);
-                        return true;
-                    }).findFirst().get().newInstance(constructorParams);
-        } catch (InstantiationException | IllegalAccessException | InvocationTargetException e) {
+            proxyInstance = proxyFactory.create(serviceDetails.getTargetConstructor().getParameterTypes(), constructorParams);
+        } catch (InstantiationException | IllegalAccessException | InvocationTargetException | NoSuchMethodException e) {
             throw new RuntimeException(e);
         }
 
